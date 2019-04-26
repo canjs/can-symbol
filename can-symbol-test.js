a3cb0f639b442bb59b72e0d07a0eae2d5011d187
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-04-26 23:24:15 +0100

    Upgrade to QUnit2

--- can-symbol-test.js
+++ can-symbol-test.js
@@ -4,15 +4,15 @@ var clone = require('steal-clone');
 
 QUnit.module('can-symbol');
 
-QUnit.test('for and keyFor', function(){
+QUnit.test('for and keyFor', function(assert) {
 	var symbol = canSymbol.for("abc");
-	QUnit.ok( canSymbol.for("abc") === canSymbol.for("abc") );
+	assert.ok( canSymbol.for("abc") === canSymbol.for("abc") );
 
-	QUnit.equal(canSymbol.keyFor(symbol), "abc", "key for");
+	assert.equal(canSymbol.keyFor(symbol), "abc", "key for");
 
 });
 
-QUnit.test("can get/set symbols", function(){
+QUnit.test("can get/set symbols", function(assert) {
 
 	var obj = {};
 
@@ -21,13 +21,13 @@ QUnit.test("can get/set symbols", function(){
 
 	obj[symbol1] = "VALUE";
 
-	QUnit.equal(obj[symbol1], "VALUE", "got value");
+	assert.equal(obj[symbol1], "VALUE", "got value");
 
 	Object.defineProperty(obj, symbol2,{
 		value: "DP-VALUE"
 	});
 
-	QUnit.equal(obj[symbol2], "DP-VALUE", "got define property value");
+	assert.equal(obj[symbol2], "DP-VALUE", "got define property value");
 
 });
 
@@ -44,7 +44,7 @@ QUnit.test("isSymbolLike with polyfill", function(assert) {
 
 	loader.import("can-symbol")
 		.then(function(canSymbol) {
-			QUnit.notEqual(canSymbol, FakeSymbol, "uses OUR polyfill");
+			assert.notEqual(canSymbol, FakeSymbol, "uses OUR polyfill");
 			window.Symbol = origSymbol;
 			done();
 		});
